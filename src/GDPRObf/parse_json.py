30c7c7fa17b7ee631a716c1dde5d51afd58528b6
JCHibbard <joshcarrick@btinternet.com>
2024-05-22 03:12:34 +0100

    Write and test parse_json

--- src/GDPRObf/parse_json.py
+++ src/GDPRObf/parse_json.py
@@ -4,5 +4,10 @@
 # Second is a list of the pii fields
 import json
 
-def parse_json(json_input):
-    pass
+def parse_json(input_json):
+    json_dict = json.loads(input_json)
+    if len(json_dict) != 2:
+        raise IndexError("Input JSON is wrong length")
+    s3_location = list(json_dict.values())[0]
+    pii_fields = list(json_dict.values())[1]
+    return s3_location, pii_fields
--- tests/test_parse_json.py
+++ tests/test_parse_json.py
@@ -2,6 +2,24 @@ import pytest
 import json
 from src.GDPRObf.parse_json import parse_json
 
-def parse_json_returns_two_outputs():
-    result = parse_json()
-    pass
+@pytest.fixture
+def input_json():
+    return json.dumps({"file_to_obfuscate" : "s3://gdpr_test_bucket/test_file.csv", "pii_fields": ["name", "email"]})
+
+def test_returns_two_outputs(input_json):
+    result = len(parse_json(input_json))
+    expected = 2
+    assert result == expected
+
+def test_returns_tuple_output(input_json):
+    result = parse_json(input_json)
+    assert isinstance(result, tuple)
+
+def test_first_tuple_item_is_s3_location(input_json):
+    result, result_2 = parse_json(input_json)
+    expected = "s3://gdpr_test_bucket/test_file.csv"
+    assert result == expected
+
+def test_second_tuple_item_is_list(input_json):
+    result, result_2 = parse_json(input_json)
+    assert isinstance(result_2, list)
