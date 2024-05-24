40d63c06629d46b6cab728734f0f3c5f436459f6
JCHibbard <joshcarrick@btinternet.com>
2024-05-24 02:40:40 +0100

    Add to-byte conversion

--- src/GDPRObf/obfuscate_fields.py
+++ src/GDPRObf/obfuscate_fields.py
@@ -1,4 +1,7 @@
-import pandas as pd
+def convert_to_bytes(obfuscated_df):
+    obfuscated_file = obfuscated_df.to_csv(index=False)
+    obfuscated_bytes = bytes(obfuscated_file, 'utf-8')
+    return obfuscated_bytes
 
 def obfuscate_fields(copied_df, pii_fields):
     obfuscated_df = copied_df
