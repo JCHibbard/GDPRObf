3f4113c78fce04575f06b70b33cbc0002b4b3280
JCHibbard <joshcarrick@btinternet.com>
2024-05-28 04:41:37 +0100

    Change conftest to use upload_file

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,15 @@ import boto3
 
 @pytest.fixture(scope="module")
 def test_dataframe():
-    return pd.read_csv('./test_data/MOCK_DATA.csv')
+    yield pd.read_csv('./test_data/MOCK_DATA.csv')
 
 @pytest.fixture(scope="module")
 def test_obfurscated_dataframe():
-    return pd.read_csv('./test_data/MOCK_DATA_OBF.csv')
+    yield pd.read_csv('./test_data/MOCK_DATA_OBF.csv')
 
 @pytest.fixture(scope="module")
 def input_json():
-    return json.dumps({"file_to_obfuscate" : "s3://gdpr_test_bucket/test_file.csv", "pii_fields": ["name", "email"]})
+    yield json.dumps({"file_to_obfuscate" : "s3://gdpr_test_bucket/test_file.csv", "pii_fields": ["name", "email"]})
 
 @pytest.fixture(scope="module")
 def aws_credentials():
@@ -37,4 +37,4 @@ def create_bucket(aws):
 
 @pytest.fixture(scope="module")
 def add_files_to_bucket(aws):
-    boto3.client("s3").put_object(Body="./test_data/MOCK_DATA.csv", Bucket="gdpr-test-bucket", Key="mock_data.csv")
+        boto3.client("s3").upload_file('./test_data/MOCK_DATA.csv', 'gdpr-test-bucket', 'mock_data.csv')
