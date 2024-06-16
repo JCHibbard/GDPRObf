4aff66cabc6add800c1eb16ed081e0a1533135d5
JCHibbard <joshcarrick@btinternet.com>
2024-06-16 03:32:25 +0100

    Update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,17 +5,37 @@ import pandas as pd
 import os
 import boto3
 
+
 @pytest.fixture(scope="function")
 def test_dataframe():
-    yield pd.read_csv('./test_data/MOCK_DATA.csv')
+    yield pd.read_csv("./test_data/MOCK_DATA.csv")
+
 
 @pytest.fixture(scope="function")
 def test_obfurscated_dataframe():
-    yield pd.read_csv('./test_data/MOCK_DATA_OBF.csv')
+    yield pd.read_csv("./test_data/MOCK_DATA_OBF.csv")
+
 
 @pytest.fixture(scope="function")
 def input_json():
-    yield json.dumps({"file_to_obfuscate" : "s3://gdpr-test-bucket/mock_data.csv", "pii_fields": ["name", "email"]})
+    yield json.dumps(
+        {
+            "file_to_obfuscate": "s3://gdpr-test-bucket/mock_data.csv",
+            "pii_fields": ["name", "email"],
+        }
+    )
+
+
+@pytest.fixture(scope="function")
+def bad_input():
+    yield json.dumps(
+        {
+            "file_to_obfuscate": "s3://gdpr-test-bucket/mock_data.csv",
+            "pii_fields": ["name", "email"],
+            "extra_key": "extra_value",
+        }
+    )
+
 
 @pytest.fixture(scope="function")
 def aws_credentials():
@@ -26,15 +46,30 @@ def aws_credentials():
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
+
 @pytest.fixture(scope="function")
 def aws(aws_credentials):
     with moto.mock_aws(aws_credentials):
         yield boto3.client("s3", region_name="eu-west-2")
 
+
 @pytest.fixture(scope="function")
 def create_bucket(aws):
-    boto3.client("s3").create_bucket(Bucket="gdpr-test-bucket", CreateBucketConfiguration={'LocationConstraint':'eu-west-2'})
+    boto3.client("s3").create_bucket(
+        Bucket="gdpr-test-bucket",
+        CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
+    )
+
 
 @pytest.fixture(scope="function")
 def add_files_to_bucket(aws):
-        boto3.client("s3").upload_file('./test_data/MOCK_DATA.csv', 'gdpr-test-bucket', 'mock_data.csv')
+    boto3.client("s3").upload_file(
+        "./test_data/MOCK_DATA.csv", "gdpr-test-bucket", "mock_data.csv"
+    )
+
+
+@pytest.fixture(scope="function")
+def add_big_files_to_bucket(aws):
+    boto3.client("s3").upload_file(
+        "./test_data/MOCK_DATA_BIG.csv", "gdpr-test-bucket", "mock_data.csv"
+    )
